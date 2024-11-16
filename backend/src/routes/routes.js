2f90a1b90896e9b7c12db59f9eb614b895467566
hazecodez <muhammedhashir298@gmail.com>
2024-11-17 00:14:47 +0530

    routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { login, register } = require("../controllers/userController");
+const {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+const protect = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/login");
+router.post("/login", login);
+router.post("/register", register);
+router.get("/tasks", protect, getTasks);
+router.post("/task", protect, createTask);
+router.put("/task/:id", protect, updateTask);
+router.delete("/task/:id", protect, deleteTask);
 
 module.exports = router;
