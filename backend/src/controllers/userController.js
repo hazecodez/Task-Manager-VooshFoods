9b323fb1d4fe4dc66fa99ecb3088427b29c1017e
hazecodez <muhammedhashir298@gmail.com>
2024-11-17 22:23:20 +0530

    bcrypt password

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
@@ -9,10 +10,9 @@ exports.register = async (req, res) => {
 
   try {
     const userExists = await User.findOne({ email });
-    if (userExists)
-      return res.json({ message: "User already exists" });
-
-    const user = await User.create({ name, email, password });
+    if (userExists) return res.json({ message: "User already exists" });
+    const hashedPass = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPass });
     res.status(201).json({ user });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
